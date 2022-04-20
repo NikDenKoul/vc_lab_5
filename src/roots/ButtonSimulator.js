1ccd893576596650da4b0227779037463c2a99af
centurion <niksoup@list.ru>
2022-04-20 10:44:25 +0300

    Добавлена возможность вернуться по кнопке на главную страницу, стилизирована сама страница /ButtonSimulator

--- src/roots/ButtonSimulator.js
+++ src/roots/ButtonSimulator.js
@@ -1,29 +1,34 @@
 import './styles.css';
+import {Link} from "react-router-dom";
 
 function ButtonSimulator() {
     return(
         <main>
-            <button>MAIN main</button>
-            <br/>
-            <box>
-                <box>
-                    <button>1</button>
-                    <button>2</button>
-                    <button>3</button>
-                </box>
+            <div className="BS-div">
+                <button>MAIN main</button>
                 <br/>
                 <box>
-                    <button>4</button>
-                    <button>5</button>
-                    <button>6</button>
+                    <box>
+                        <button>1</button>
+                        <button>2</button>
+                        <button>3</button>
+                    </box>
+                    <br/>
+                    <box>
+                        <button>4</button>
+                        <button>5</button>
+                        <button>6</button>
+                    </box>
+                    <br/>
+                    <box>
+                        <button>7</button>
+                        <button>8</button>
+                        <button>9</button>
+                    </box>
                 </box>
                 <br/>
-                <box>
-                    <button>7</button>
-                    <button>8</button>
-                    <button>9</button>
-                </box>
-            </box>
+                <button><Link to="/">Go back, I want to be monkey</Link></button>
+            </div>
         </main>
     )
 }
