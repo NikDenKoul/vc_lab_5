bf8b65919cfe4ab241ef145cd8768456d26ea0f9
centurion <niksoup@list.ru>
2022-04-20 10:32:29 +0300

    Реализована форма авторизации по адресу /InputSimulator

--- src/roots/InputSimulator.js
+++ src/roots/InputSimulator.js
@@ -1,9 +1,24 @@
-function ButtonSimulator() {
+import "./Form.css"
+
+function InputSimulator() {
     return(
         <main>
-            <input type="text" name="name" />
+            <form className="authorization-form" id="authorizationForm">
+                <h1 align="center">Авторизация:</h1>
+
+                <input className="authorization-element" name="authorizationUsername" id="authorizationUsername"
+                       placeholder="Имя пользователя">
+                </input>
+                <input className="authorization-element" name="authorizationPassword" id="authorizationPassword"
+                       placeholder="Пароль">
+                </input>
+
+                <button type="submit" className="authorization-element authorization-button">
+                    Авторизоваться
+                </button>
+            </form>
         </main>
     )
 }
 
-export default ButtonSimulator;
+export default InputSimulator;
