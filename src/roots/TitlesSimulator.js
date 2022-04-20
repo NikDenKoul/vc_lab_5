e5e0bae8b5f0f7ce3c5300ca88068fde21d6b463
centurion <niksoup@list.ru>
2022-04-20 10:05:55 +0300

    Затонирована тема, элементы распологаются по центру

--- src/roots/TitlesSimulator.js
+++ src/roots/TitlesSimulator.js
@@ -1,9 +1,13 @@
+import "./TitlesSimulator.css"
+
 function TitlesSimulator() {
     return(
         <main>
-            <h1>MAIN main</h1>
-            <h2>This is titles simulator</h2>
-            <h3>There are some titles demonstrated here</h3>
+            <header class="TitlesSimulator-header">
+                <h1>MAIN main</h1>
+                <h2>This is titles simulator</h2>
+                <h3>There are some titles demonstrated here</h3>
+            </header>
         </main>
     )
 }
