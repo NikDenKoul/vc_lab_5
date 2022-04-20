863ecd1102dad6b3828b3a4c19a79db6e2460d7f
centurion <niksoup@list.ru>
2022-04-20 09:28:01 +0300

    Добавлены два заголовка h2 и h3 на странице TitlesSimulator

--- src/roots/TitlesSimulator.js
+++ src/roots/TitlesSimulator.js
@@ -2,6 +2,8 @@ function TitlesSimulator() {
     return(
         <main>
             <h1>MAIN main</h1>
+            <h2>This is titles simulator</h2>
+            <h3>There are some titles demonstrated here</h3>
         </main>
     )
 }
