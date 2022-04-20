ab9d6c2d857d0aa91e6b22e06469ca77094502b1
centurion <niksoup@list.ru>
2022-04-20 09:44:07 +0300

    Меню навигации перенесено наверх, ориентация по правой стороне

--- src/App.js
+++ src/App.js
@@ -5,16 +5,21 @@ import {Link} from "react-router-dom";
 function App() {
   return (
     <div id="App" className="App">
+        <div class="heading">
+            <nav text-align="left">
+                <ul>
+                    <li><Link to="/TitleSimulator">TitleSimulator</Link></li>
+                    <li><Link to="/ButtonSimulator">ButtonSimulator</Link></li>
+                    <li><Link to="/InputSimulator">InputSimulator</Link></li>
+                </ul>
+            </nav>
+        </div>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <nav>
-            <Link to="/TitleSimulator">TitleSimulator</Link>
-            <Link to="/ButtonSimulator">ButtonSimulator</Link>
-            <Link to="/InputSimulator">InputSimulator</Link>
-        </nav>
       </header>
     </div>
   );
