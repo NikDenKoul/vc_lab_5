e3adb26418e5e7cfd81978ca85c59a921d55a2cc
centurion <niksoup@list.ru>
2022-04-20 09:57:18 +0300

    Добавлено 9 кнопок с цифрами от 1 до 9 в сетке 3 на 3

--- src/roots/ButtonSimulator.js
+++ src/roots/ButtonSimulator.js
@@ -1,7 +1,29 @@
+import './styles.css';
+
 function ButtonSimulator() {
     return(
         <main>
             <button>MAIN main</button>
+            <br/>
+            <box>
+                <box>
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                </box>
+                <br/>
+                <box>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                </box>
+                <br/>
+                <box>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                </box>
+            </box>
         </main>
     )
 }
