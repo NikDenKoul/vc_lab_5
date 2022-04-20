afc9c507731cf0b02150b6d8071b7c28d390164b
centurion <niksoup@list.ru>
2022-04-20 11:01:39 +0300

    Реализовано ВОЗВРАЩЕНИЕ ДОМОЙ со страницы /TitlesSimulator

--- src/roots/TitlesSimulator.js
+++ src/roots/TitlesSimulator.js
@@ -1,4 +1,5 @@
 import "./TitlesSimulator.css"
+import {Link} from "react-router-dom"
 
 function TitlesSimulator() {
     return(
@@ -7,6 +8,12 @@ function TitlesSimulator() {
                 <h1>MAIN main</h1>
                 <h2>This is titles simulator</h2>
                 <h3>There are some titles demonstrated here</h3>
+                <box>
+                    <h5>"Любовью чужой горят города</h5>
+                    <h5>Извилистый путь затянулся петлёй</h5>
+                    <h5>Когда все дороги ведут в никуда</h5>
+                    <h4><Link to="/">НАСТАЛА ПОРА ВОЗВРАЩАТЬСЯ ДОМОЙ"</Link></h4>
+                </box>
             </header>
         </main>
     )
